6d7f3155ae6ac2650503af40d38f86244eb6c406
Shantanu <shantanu@render.com>
2021-05-04 15:54:57 -0700

    log results 5% of the time

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const postgres = require('postgres');
 const sql = postgres(); 
 
 async function getCourses() {
-   await sql`
+   const result = await sql`
     SELECT * FROM courses
   `.catch(x => console.log("Error", x)); 
+  if (Math.random() < 0.05) {
+    console.log("returned rows: ", result.count); 
+  }
 }
 
 setInterval(getCourses, 1000);

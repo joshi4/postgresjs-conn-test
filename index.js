9c54a90558999732c350ddf2b25d55410fa230f7
Shantanu <shantanu@render.com>
2021-05-04 15:22:35 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const postgres = require('postgres'); 
+
+const sql = postgres({ssl: require}); 
+
+async function getCourses() {
+   await sql`
+    SELECT * FROM courses
+  `.catch(x => console.log("Error", x)); 
+}
+
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);
+setInterval(getCourses, 1000);

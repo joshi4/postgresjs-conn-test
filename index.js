cb8490ac7fc9e83cd804b69bc425bbf0ad21cbdb
Shantanu <shantanu@render.com>
2021-05-04 19:25:55 -0700

    restructure

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const postgres = require('postgres'); 
+const postgres = require('postgres');
 
-const sql = postgres(); 
+const sql = postgres();
 
 async function getCourses() {
+  try {
    const result = await sql`
     SELECT * FROM courses
-  `.catch(x => console.log("Error", x)); 
-  if (Math.random() < 0.05) {
-    console.log("returned rows: ", result.count); 
+  `;
+
+    if (result && Math.random() < 0.05) {
+      console.log("returned rows: ", result.count);
+    }
+  } catch(x) {
+   console.log("Error", x);
   }
 }
 
-setInterval(getCourses, 10000);
-setInterval(getCourses, 30000);
-setInterval(getCourses, 60000);
-setInterval(getCourses, 1000000);
+
+
+function startLoop() {
+  ( async ()=>{
+    setInterval(getCourses, 1000);
+    setInterval(getCourses, 2000);
+  })();
+}
+
+startLoop();
+
+// setInterval(getCourses, 60000);
+// setInterval(getCourses, 1000000);

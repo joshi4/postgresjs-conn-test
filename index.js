ca3832cd4fac86ff0773f0ec40fdb555883fa38b
Shantanu <shantanu@render.com>
2021-06-18 11:16:15 -0700

    redce idle_timeout to one minute.
    queries run once every 35 seconds.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const postgres = require('postgres');
 
 const sql = postgres({
- idle_timeout: 60 * 60
+ idle_timeout: 60
 });
 
 async function getCourses(name) {
@@ -30,6 +30,7 @@ function startLoop() {
     await getCourses("8");
     await getCourses("9");
     await getCourses("10");
+    await getCourses("11");
 
     await getCourses("12");
     await getCourses("13");
@@ -43,27 +44,27 @@ function startLoop() {
 
    console.log("initial calls end");
 
-    setInterval(()=>{getCourses("1")},  3000 * 1000);
-    setInterval(()=>{getCourses("2")},  3000 * 1000);
-    setInterval(()=>{getCourses("3")},  3000 * 1000);
-    setInterval(()=>{getCourses("4")},  3000 * 1000);
-    setInterval(()=>{getCourses("5")},  3000 * 1000);
-    setInterval(()=>{getCourses("6")},  3000 * 1000);
-    setInterval(()=>{getCourses("7")},  3000 * 1000);
-    setInterval(()=>{getCourses("8")},  3000 * 1000);
-    setInterval(()=>{getCourses("9")},  3000 * 1000);
-    setInterval(()=>{getCourses("10")},  3000 * 1000);
+    setInterval(()=>{getCourses("1")},  35 * 1000);
+    setInterval(()=>{getCourses("2")},  35 * 1000);
+    setInterval(()=>{getCourses("3")},  35 * 1000);
+    setInterval(()=>{getCourses("4")},  35 * 1000);
+    setInterval(()=>{getCourses("5")},  35 * 1000);
+    setInterval(()=>{getCourses("6")},  35 * 1000);
+    setInterval(()=>{getCourses("7")},  35 * 1000);
+    setInterval(()=>{getCourses("8")},  35 * 1000);
+    setInterval(()=>{getCourses("9")},  35 * 1000);
+    setInterval(()=>{getCourses("10")},  35 * 1000);
 
-    setInterval(()=>{getCourses("11")},  3000 * 1000);
-    setInterval(()=>{getCourses("12")},  3000 * 1000);
-    setInterval(()=>{getCourses("13")},  3000 * 1000);
-    setInterval(()=>{getCourses("14")},  3000 * 1000);
-    setInterval(()=>{getCourses("15")},  3000 * 1000);
-    setInterval(()=>{getCourses("16")},  3000 * 1000);
-    setInterval(()=>{getCourses("17")},  3000 * 1000);
-    setInterval(()=>{getCourses("18")},  3000 * 1000);
-    setInterval(()=>{getCourses("19")},  3000 * 1000);
-    setInterval(()=>{getCourses("20")},  3000 * 1000);
+    setInterval(()=>{getCourses("11")},  35 * 1000);
+    setInterval(()=>{getCourses("12")},  35 * 1000);
+    setInterval(()=>{getCourses("13")},  35 * 1000);
+    setInterval(()=>{getCourses("14")},  35 * 1000);
+    setInterval(()=>{getCourses("15")},  35 * 1000);
+    setInterval(()=>{getCourses("16")},  35 * 1000);
+    setInterval(()=>{getCourses("17")},  35 * 1000);
+    setInterval(()=>{getCourses("18")},  35 * 1000);
+    setInterval(()=>{getCourses("19")},  35 * 1000);
+    setInterval(()=>{getCourses("20")},  35 * 1000);
   })();
 }
 
